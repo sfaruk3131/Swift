d33a77adffad3e454cd25f59fd2e3ec4d3331815
Xander <acox9567@bths.edu>
2019-02-01 10:14:07 -0500

    asdf

--- src/sample/Algorithms.java
+++ src/sample/Algorithms.java
@@ -4,5 +4,25 @@
 package sample;
 
 public class Algorithms {
-    
+
+    private String DDG_URL1 = "https://duckduckgo.com/?q=", DDG_URL2 = "&t=hi";
+
+    private String question = "";
+
+    void setupClient()
+    {
+        WebClient client = new WebClient();
+        client.getOptions().setCssEnabled(false);
+        client.getOptions().setJavaScriptEnabled(false);
+    }
+
+    int FullSearch(String query)
+    {
+
+    }
+
+    int QuickSearch(String query)
+    {
+
+    }
 }
